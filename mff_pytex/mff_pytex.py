4deca5441bf07699a6d767332532831012b41926
Ondřej Chwiedziuk <ondrachwiedziuk@gmail.com>
2022-04-06 11:56:33 +0200

    Test docs

--- mff_pytex/mff_pytex.py
+++ mff_pytex/mff_pytex.py
@@ -1,1 +1,16 @@
 """Main module."""
+
+def test_docs(hello: str) -> str:
+    """Test Docs
+
+    Testing how docs writing works
+
+    Args:
+        hello (str): String which should be printed
+
+    Returns:
+        str: Printed string
+    """
+
+    print(hello)
+    return hello
