4c7e9c7d9d021db588343be487fefc0ac59459c0
Ondřej Chwiedziuk <ondrachwiedziuk@gmail.com>
2022-04-06 11:05:04 +0200

    Test testing

--- tests/test_mff_pytex.py
+++ tests/test_mff_pytex.py
@@ -22,3 +22,6 @@ def test_content(response):
     """Sample pytest test function with the pytest fixture as an argument."""
     # from bs4 import BeautifulSoup
     # assert 'GitHub' in BeautifulSoup(response.content).title.string
+
+def test_testing():
+    assert True
